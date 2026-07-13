765c5bb3fd761ac8ec0a4908f2a94ebefecdee51
DMK98 <164352152+DMK98@users.noreply.github.com>
2026-07-13 18:42:50 +0530

    Add files via upload

---
+++ 7 loop.js
@@ -0,0 +1,52 @@
+"use strict"
+let i = 11;
+/*if(i<10){
+    console.log(i);    
+}*/
+
+/*console.time("t");
+while(i<10){
+    // i++;
+    // ++i;
+    console.log(i);    
+    ++i;
+}
+console.timeEnd("t");*/
+
+/*do{
+    console.log(i++);    
+}while(i<10)*/
+
+
+/*for(;i<=10; i++){
+    console.log(i);    
+}*/
+
+/*for(let t=2;t<=20; t=t+2){
+    console.log(t);    
+}*/
+
+/*let count = 0
+for (let i = 1; i <= 100; i = i + i) {
+    console.log(i);
+    count++;
+}
+console.log("iteration count >>",count);*/
+
+/*for (let i = 1; i < 100; i++) {
+    if (i % 23 === 0) {
+        console.log(i);
+        break;
+    }
+}*/
+
+/*for(let i =1; i<=100;i++){
+    if(i%10===0){continue};
+    console.log(i);    
+}*/
+
+for(let i=1;i<=5;i++ ){
+    document.querySelector("main ol").innerHTML+="<li>List item</li>"
+
+}
+38
