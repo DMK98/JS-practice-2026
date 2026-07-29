974a7ce891a6ba39acab7bcf300358a6f30d434c
DMK98 <164352152+DMK98@users.noreply.github.com>
2026-07-29 17:41:18 +0530

    Add files via upload

--- 11 js class and promises.js
+++ 11 js class and promises.js
@@ -1,5 +1,5 @@
-const x= new Array();
-const y=new Object();
+// const x= new Array();
+// const y=new Object();
 
 /*function Car(name,power,torque,price){
     this.name=name;
@@ -96,10 +96,77 @@ console.log(swift);*/
 const swift=new Car("swift", 8000);
 console.log(swift);*/
 
-class Car{
+/*class Car{
     constructor(name,price){
             this.name=name;
             this.price=price;
     }
-    start()
+    start(){
+
+    }
+}*/
+
+// class Car{
+//     constructor(name,power,torque, price){
+//         this.name=name;
+//         this.torque=torque;
+//         this.torque=torque;
+//         this.price=price;
+//     }
+// };
+// const swift = new Car("alto", 40,50,100000);
+
+
+// class declaration
+/*class Car {
+    constructor(power, torque) {
+        this.power = power;
+        this.torque = torque
+    }
+};
+const swift = new Car(90, 115)
+console.log(swift);*/
+
+// Class expression
+/*const Car = class {
+    constructor(power, torque) {
+        this.power = power;
+        this.torque=torque;
+    }
+    start(){return this.name + "start"}
+
+};*/
+// const maruti=new Car(50,250)
+// console.log(maruti);
+// console.log(Car());
+
+class Car {
+    constructor(name, power, torque, price) {
+        this.name = name;
+        this.power = power;
+        this.torque = torque;
+        this.price = price;
+    }
+};
+const swift = new Car("alto", 40, 50, 100000);
+// console.log(swift);
+
+class Suzuki extends Car {
+    w=2;
+    constructor(name, power, torque, price) {
+        super(name, power, torque, price);
+    }
 }
+class Toyota extends Car {
+    w=3;
+    constructor(name, power, torque, price) {
+        super(name, power, torque, price);
+    }
+}
+
+const nexa= new Suzuki("nexa",100,80,100000);
+const breza= new Suzuki("breza",200,160,200000);
+const fortuner= new Toyota("fortuner",300,240,300000);
+console.log(fortuner);
+console.log(breza);
+console.log(nexa);
