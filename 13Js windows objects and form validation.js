87a53bbb5e605132026fca1fef39dfdc4315d0a1
DMK98 <164352152+DMK98@users.noreply.github.com>
2026-08-18 19:01:18 +0530

    Add files via upload

---
+++ 13Js windows objects and form validation.js
@@ -0,0 +1,17 @@
+"use strict"
+/*window.onload=function(){
+    alert("hello")
+}*/
+
+// document.addEventListener("DOMContentLoaded",alert("ffa"));
+
+/*document.querySelector(".open").addEventListener("click",function(){
+    window.open("./4.html","_blank","width=400 height=400")
+})*/
+
+// console.log(location.href);
+// console.log(location.origin);
+// console.log(location.host);
+// console.log(location.hostname);
+
+
