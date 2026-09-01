2438acbb370c835b98467f559224ff98df5e3d6a
DMK98 <164352152+DMK98@users.noreply.github.com>
2026-09-01 18:52:31 +0530

    Add files via upload

--- 13Js windows objects and form validation.js
+++ 13Js windows objects and form validation.js
@@ -15,3 +15,70 @@
 // console.log(location.hostname);
 
 
+// const str="lorem ipsum1";
+// const p=/\S/i;
+// const p=/\s/;
+// const p=/\d/;
+
+// console.log(p.test(str));
+
+// ============NUMber============
+document.querySelector(".pincode").addEventListener("input", function () {
+    const x = this.value;
+    const p = /^[1-9]{6}$/;
+    if (p.test(x)) {
+        document.querySelector(".ans").innerHTML = "match"
+    } else {
+        document.querySelector(".ans").innerHTML = "not match"
+    }
+})
+// ============Email============
+document.querySelector(".email").addEventListener("input", function () {
+    const x = this.value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,3}$/;
+    //  const emailRegex = /^[\w]{1}\@\.$/;
+    if (emailRegex.test(x)) {
+        document.querySelector(".emailAns").innerHTML = "match"
+    } else {
+        document.querySelector(".emailAns").innerHTML = "not match"
+    }
+})
+// ============password============
+document.querySelector(".password").addEventListener("input", function () {
+    const x = this.value;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+-.])(?=.{8,12})/;
+    if (passwordRegex.test(x)) {
+        document.querySelector(".passwordAns").innerHTML = "match"
+    } else {
+        document.querySelector(".passwordAns").innerHTML = "not match"
+    }
+})
+
+// ===================Sigup=======================
+document.signup.addEventListener("submit", function (e) {
+    this.name.nextElementSibling.innerHTML = "";
+    this.age.nextElementSibling.innerHTML = "";
+
+    if (this.name.value.trim() == "") {
+        e.preventDefault();
+        this.name.nextElementSibling.innerHTML = "Enter name";
+        this.name.nextElementSibling.style = "color:red";
+        this.name.focus();
+    } else if (this.age.value.trim() == "") {
+        e.preventDefault();
+        this.age.nextElementSibling.innerHTML = "Enter  age";
+        this.age.nextElementSibling.style = "color:orange";
+        this.age.focus();
+    } else if (this.age.value < 18 || this.age.value > 25) {
+        e.preventDefault();
+        this.age.nextElementSibling.innerHTML = "Enter valid age";
+        this.age.nextElementSibling.style = "color:red";
+        this.age.focus();
+    // } else if (this.resume.value=="") {
+    } else if (this.resume.files[0].size > 1048576) {
+        e.preventDefault();
+        this.resume.nextElementSibling.innerHTML = "Upload resume";
+        this.resume.nextElementSibling.style = "color:red";
+        this.resume.focus();
+    }
+})
