4702627c30db7aa75005605154da7b5d8004cc16
DMK98 <164352152+DMK98@users.noreply.github.com>
2026-09-03 18:48:18 +0530

    Add files via upload

---
+++ 15Jquery.js
@@ -0,0 +1,14 @@
+"use strict"
+
+/*document.querySelector("input").addEventListener("input",function(){
+    this.nextElementSibling.innerHTML=this.value;
+})*/
+
+// console.log(jQuery.fn.jquery);
+// console.log($);
+// console.log(jQuery);
+// $(".message");
+// document.querySelector(".message").textContent = "Welcome";
+
+// $("p").text("Welcome");
+// $("p").text("manish")
